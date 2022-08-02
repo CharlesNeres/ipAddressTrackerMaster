2c0925d820a5127f478aaca6b0050d57684a4b40
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-08-02 23:25:00 +0800

    implemented main structure the site (html, css)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <h1>IP address tracker</h1>
+        <form>
+          <input type="text" />
+          <button type="submit">&#10095;</button>
+        </form>
+        <main>
+          <ul>
+            <li>
+              <span>ip address</span>
+              <p>192.212.174.101</p>
+            </li>
+            <li>
+              <span>location</span>
+              <p>Brooklyn, NY 10001</p>
+            </li>
+            <li>
+              <span>timezone</span>
+              <p>UTC -05:00</p>
+            </li>
+            <li>
+              <span>isp</span>
+              <p>SpaceX Starlink</p>
+            </li>
+          </ul>
+        </main>   
+      </header>
+         
+      <div className='map'></div>
     </div>
   );
 }
