a36cbd361c94eb54105171308ff7bab43a6541eb
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-08-13 23:01:33 +0800

    created function to load datas from IP Geolocation API

--- src/App.js
+++ src/App.js
@@ -1,39 +1,77 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>IP address tracker</h1>
-        <form>
-          <input type="text" />
-          <button type="submit">&#10095;</button>
-        </form>
-        <main>
-          <ul>
-            <li>
-              <span>ip address</span>
-              <p>192.212.174.101</p>
-            </li>
-            <li>
-              <span>location</span>
-              <p>Brooklyn, NY 10001</p>
-            </li>
-            <li>
-              <span>timezone</span>
-              <p>UTC -05:00</p>
-            </li>
-            <li>
-              <span>isp</span>
-              <p>SpaceX Starlink</p>
-            </li>
-          </ul>
-        </main>   
-      </header>
-         
-      <div className='map'></div>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.loadIPGeolocationAPI = this.loadIPGeolocationAPI.bind(this);
+    this.loadDataIPGeolocationAPI = this.loadDataIPGeolocationAPI.bind(this);
+    this.state = {}
+  }
+
+  loadIPGeolocationAPI(e){
+    e.preventDefault();
+    let ip = e.target[0].value;
+
+    let api_key = 'at_gKGA2OV6BNuarwVr7e8sr11qAPguc';
+    var api_url = 'https://geo.ipify.org/api/v1?';    
+    var url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip;
+    
+    if(ip){
+      fetch(url)
+      .then(response => response.json())
+      .then(data => this.loadDataIPGeolocationAPI(data));
+    }
+  }
+
+  loadDataIPGeolocationAPI(data){
+    let location = data.location;
+    this.setState({
+      ip: data.ip,
+      isp: data.isp,
+      city: location.city,
+      country: location.country,
+      postalCode: location.postalCode,
+      region: location.region,
+      timezone: location.timezone
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>IP address tracker</h1>
+          <form onSubmit={this.loadIPGeolocationAPI}>
+            <input type="text" />
+            <button type="submit">&#10095;</button>
+          </form>
+          <main>
+            <ul>
+              <li>
+                <span>ip address</span>
+                <p>{this.state.ip}</p>
+              </li>
+              <li>
+                <span>location</span>
+                <p>{this.state.city}, {this.state.region} {this.state.postalCode}</p>
+              </li>
+              <li>
+                <span>timezone</span>
+                <p>UTC {this.state.timezone}</p>
+              </li>
+              <li>
+                <span>isp</span>
+                <p>{this.state.isp}</p>
+              </li>
+            </ul>
+          </main>   
+        </header>
+          
+        <div className='map'></div>
+      </div>
+    );
+  }
 }
 
 export default App;
